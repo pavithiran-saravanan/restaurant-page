d10110050f707add1f289d02c232b590f9eed972
pavithiran <pavithransaravanan22@gmail.com>
2023-07-04 08:49:26 +0530

    Modify webpack config to make asset loader work with custom fonts

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './style.css';
 
 // Create 3 button -> home, menu and contact. Add event listeners to load the crct page content.
 const content = document.querySelector('.content');
-const buttons = document.createElement('div'); 
-const homeButton = document.createElement('button'); homeButton.textContent = 'Home';
-const menuButton = document.createElement('button'); menuButton.textContent = 'Menu';
-const contactButton = document.createElement('button'); contactButton.textContent = 'Contact';
+const stickyHeader = document.createElement('header'); stickyHeader.classList.add('sticky-header');
+const navBar = document.createElement('ul'); navBar.classList.add('nav-bar');
+const homeButton = document.createElement('li'); homeButton.textContent = 'Home';
+const menuButton = document.createElement('li'); menuButton.textContent = 'Menu';
+const contactButton = document.createElement('li'); contactButton.textContent = 'Contact';
 
 homeButton.addEventListener('click', (e) => {
     clear();
@@ -25,8 +26,10 @@ contactButton.addEventListener('click', (e) => {
     contact();
 });
 
-buttons.append(homeButton, menuButton, contactButton);
-content.appendChild(buttons);
+navBar.append(homeButton, menuButton, contactButton);
+stickyHeader.appendChild(navBar);
+content.appendChild(stickyHeader);
+
 const pageContent = document.createElement('div'); pageContent.classList.add('page-content');
 content.appendChild(pageContent);
 home();
